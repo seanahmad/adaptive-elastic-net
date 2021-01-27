e35b1627981adf0c67c07752aa72b861c6c09766
Shota Imaki <shota.imaki@icloud.com>
2021-01-27 09:54:22 +0900

    update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/sh -eu
 
-python3 -m pytest --doctest-modules adaptive_enet
+python3 -m pytest --doctest-modules aenet
 python3 -m pytest --doctest-modules tests
 
-python3 -m flake8 adaptive_enet
-python3 -m black --check adaptive_enet || read -p "Run formatter? (y/N): " yn; [[ $yn = [yY] ]] && python3 -m black adaptive_enet
-python3 -m isort --check --force-single-line-imports adaptive_enet || read -p "Run formatter? (y/N): " yn; [[ $yn = [yY] ]] && python3 -m isort --force-single-line-imports adaptive_enet
+python3 -m flake8 aenet
+python3 -m black --check aenet || read -p "Run formatter? (y/N): " yn; [[ $yn = [yY] ]] && python3 -m black aenet
+python3 -m isort --check --force-single-line-imports aenet || read -p "Run formatter? (y/N): " yn; [[ $yn = [yY] ]] && python3 -m isort --force-single-line-imports aenet
